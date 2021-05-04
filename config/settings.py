622623f7072c8713392957d78a8d89156b550b18
Nick Warchol <nwarchol123@icloud.com>
2021-05-03 18:07:16 -0700

    Commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v4)fo&3lw)1zfgtq9naw-3*uoys*2h+ug!k%f%14p7=juo^#-6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath("templates"))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from .models import Post
 
 class HomePageView(ListView):
     model = Post
-    template - "home.html"
+    template_name = "home.html"
+    context_object_name = "all_posts_list"
 
 
